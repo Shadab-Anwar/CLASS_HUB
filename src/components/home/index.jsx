fb4947d2ea52964f8e1b4da97c7dbc0c45300d3a
Shadab Anwar <shadabanwar1810@gmail.com>
2025-02-21 23:32:06 +0530

    Update index.jsx

--- src/components/home/index.jsx
+++ src/components/home/index.jsx
@@ -9,7 +9,6 @@ function Home() {
 
   const [Show , setShow] = useState(true);
 
-
   return (
     <>
       <nav className="bg-white dark:bg-gray-900 fixed w-full z-20 top-0 start-0 border-b border-gray-200 dark:border-gray-600 ">
